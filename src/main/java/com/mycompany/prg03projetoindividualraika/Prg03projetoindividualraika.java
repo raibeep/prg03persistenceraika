7a399f35fce0d179fa2d5c71bed70c2290dbbdd8
raibeep <20242ireads0002@ifba.edu.br>
2025-11-04 20:17:04 -0300

    primeiro commit local

---
+++ src/main/java/com/mycompany/prg03projetoindividualraika/Prg03projetoindividualraika.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prg03projetoindividualraika;
+
+/**
+ *
+ * @author raiii
+ */
+public class Prg03projetoindividualraika {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
