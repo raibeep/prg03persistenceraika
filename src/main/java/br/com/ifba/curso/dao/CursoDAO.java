74895c1bef50b52f09828669ef24977b83a0e204
raibeep <20242ireads0002@ifba.edu.br>
2025-11-08 20:09:08 -0300

    cria CursoDAO

---
+++ src/main/java/br/com/ifba/curso/dao/CursoDAO.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.curso.dao;
+//importa as classes de outros pacotes que desejo usar aqui
+import br.com.ifba.curso.util.JPAUtil;
+import br.com.ifba.curso.entity.Curso;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+/**
+ *
+ * @author raiii
+ */
+//o DAO é feito para armazenar a lógica e fazer essa conexão entre as classes
+//é aqui onde eu vou criar os métodos salvar, editar, excluir e etc.
+public class CursoDAO {
+    public void salvarCurso(Curso curso){
+        EntityManager em = JPAUtil.getEntityManager(); //cria a conexão com o banco de dados
+        try{
+            em.getTransaction().begin();
+            em.persist(curso);//salva
+            em.getTransaction().commit();
+        }catch(Exception e){
+            em.getTransaction().rollback();
+            throw e;
+        }finally{
+            em.close();
+        }
+    }
+    
+    public void removerCurso(Curso curso){
+        EntityManager em = JPAUtil.getEntityManager();
+        em.getTransaction().begin();
+        
+        Curso c = em.find(Curso.class, curso.getId());//busca o curso e c vai apontar para o curso encontrado
+        if(c != null){
+            em.remove(c);//remove do banco
+        }
+        
+        em.getTransaction().commit();
+        em.close();
+    }
+    
+    public void atualizarCurso(Curso curso){
+        EntityManager em = JPAUtil.getEntityManager();
+        try{
+            em.getTransaction().begin();
+            em.merge(curso);//atualiza os dados
+            em.getTransaction().commit();
+        }catch(Exception e){
+            em.getTransaction().rollback();
+            throw e;
+        } finally{//bloco executado sempre
+            em.close();
+        }    
+    }
+    
+    public List<Curso> buscarTodos(){
+        EntityManager em = JPAUtil.getEntityManager();
+        TypedQuery<Curso> query = em.createQuery("SELECT c FROM Curso c WHERE c.ativo = true", Curso.class);//cria consulta JPQL (seleciona todos os objetos do tipo Curso que estão no banco
+        List<Curso> cursos = query.getResultList();//armazena todos os obejtos em curso, criando uma lista
+        em.close();
+        return cursos;//retorna a lista
+    }
+}
