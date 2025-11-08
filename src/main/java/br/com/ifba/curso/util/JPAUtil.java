63cf3af85c468d85023cbd4b626ef68645bab436
raibeep <20242ireads0002@ifba.edu.br>
2025-11-08 20:07:40 -0300

    cria o JPAUtil

---
+++ src/main/java/br/com/ifba/curso/util/JPAUtil.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.curso.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+/**
+ *
+ * @author raiii
+ */
+//essa classe é feita para fazer a conexão com o banco de dados e facilitar o uso do EntityManager 
+public class JPAUtil {
+        //cria uma fábrica de conexões JPA
+    private final static EntityManagerFactory entityManagerFactory = 
+            Persistence.createEntityManagerFactory("gerenciamento_curso");
+    //cria um gerenciador de entidades, ou seja, o obejto que faz as operações de CRUD
+    //(salvar, buscar, remover, atualizar) no banco.
+    private final static EntityManager entityManager = 
+            entityManagerFactory.createEntityManager();
+
+    public static EntityManager getEntityManager() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
